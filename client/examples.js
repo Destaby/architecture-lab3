8135ba0e0b596c86f24102b7f3052d873a8bc97a
NikitaLitvishko <litvishko.nikita@mail.ru>
2021-11-28 20:21:16 +0200

    fixing examples

--- client/examples.js
+++ client/examples.js
@@ -6,27 +6,27 @@ const system = require('./system/client');
 const client = system.Client('http://localhost:8080');
 
 // Scenario 1: Display balancers.
-// client
-//   .listBalancers()
-//   .then(list => {
-//     console.log('=== Scenario 1 ===');
-//     console.log('Balancers:');
-//     console.log(list);
-//   })
-//   .catch(e => {
-//     console.log(`Problem listing balancers: ${e.message}`);
-//   });
-
-// Scenario 2: Update working status.
 client
-  .updateWorkingStatus(1, true)
-  .then(resp => {
-    console.log('=== Scenario 2 ===');
-    console.log('Update working working status of a machine:', resp);
-    // return client
-    //   .listBalancers()
-    //   .then(list => console.log('Current Balancers:', list));
+  .listBalancers()
+  .then(list => {
+    console.log('=== Scenario 1 ===');
+    console.log('Balancers:');
+    console.log(list);
   })
   .catch(e => {
-    console.log(`Problem updating status of a machine: ${e.message}`);
+    console.log(`Problem listing balancers: ${e.message}`);
   });
+
+//Scenario 2: Update working status.
+client
+   .updateWorkingStatus(1, true)
+   .then(resp => {
+     console.log('=== Scenario 2 ===');
+     console.log('Update working working status of a machine:', resp);
+        return client
+        .listBalancers()
+        .then(list => console.log('Current Balancers:', list));
+   })
+   .catch(e => {
+     console.log(`Problem updating status of a machine: ${e.message}`);
+   });
